1e8ab205e662125701eca276a39ab826727acd30
rui-watanabe <wataru610806@gmail.com>
2020-05-29 21:21:31 +0900

    set strict mode

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,2 +1,23 @@
-let hi = 'hi';
-console.log(hi.toUpperCase());
+function echo(message: string): string | null{
+  return message;
+}
+
+let implicitAny;
+implicitAny = 'implicitAny';
+implicitAny.toUpperCase();
+
+// let nullableMessage: string = null;
+// let undefinedMessage: string = undefined;
+// let onlyNull: null = undefined;
+// let onlyUndefined: undefined = null;
+let nullableMessage = echo('hi');
+let undefinedMessage: string | undefined = undefined;
+let onlyNull: null = null;
+let onlyUndefined: undefined = undefined;
+
+// nullableMessage.toUpperCase
+if(nullableMessage) {
+  nullableMessage.toUpperCase();
+}
+
+const newFunction = echo.call(null, 'hi');
