019801a309472b63499a62d144e6cd2216332c24
rui-watanabe <wataru610806@gmail.com>
2020-05-30 14:40:18 +0900

    type assersion

--- src/advanced.ts
+++ src/advanced.ts
@@ -81,3 +81,9 @@ function havePet(pet: Pet) {
   }
 }
 havePet(new Bird());
+
+
+// const input = <HTMLInputElement>document.getElementById('input');
+// const input = document.getElementById('input') as HTMLInputElement;
+// input.value = 'initial input value';
+(document.getElementById('input') as HTMLInputElement).value = 'initial input value';
