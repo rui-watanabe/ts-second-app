83bd88b28b32cf006b63540947de30915a0a6c25
rui-watanabe <wataru610806@gmail.com>
2020-05-30 14:20:40 +0900

    type gurard

--- src/advanced.ts
+++ src/advanced.ts
@@ -31,3 +31,46 @@ const max: EnginnerAndBlogger = {
 type NumberOrBoolean = number | boolean;
 type StringOrNumber = string | number;
 type Mix = NumberOrBoolean & StringOrNumber;
+
+function toUpperCase(x: string | number) {
+  if(typeof x === 'string'){
+    return x.toUpperCase();
+  }
+  return '';
+}
+
+type NomadWorker = Enginner | Blogger;
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name);
+  if('role' in nomadWorker){
+    console.log(nomadWorker.role);
+  }
+  if('follower' in nomadWorker){
+    console.log(nomadWorker.follower);
+  }
+}
+
+class Dog {
+  speak() {
+    console.log('Bau Bau')
+  }
+}
+
+class Bird {
+  speak() {
+    console.log('pio pio');
+  }
+  fly() {
+    console.log('flutter');
+  }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+  pet.speak();
+  // if('fly' in pet){
+  if(pet instanceof Bird){
+    console.log(pet.fly());
+  }
+}
+havePet(new Bird());
