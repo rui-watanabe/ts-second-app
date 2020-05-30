186d88bdad17be4ab8769ff4e8a94b6da02630be
rui-watanabe <wataru610806@gmail.com>
2020-05-30 09:03:36 +0900

    add greeting method in Person class

--- src/class.ts
+++ src/class.ts
@@ -1,9 +1,22 @@
 class Person {
   name: string;
+
   constructor(initName: string) {
     this.name = initName;
-  } 
+  }
+
+  greeting(this: { name: string }){
+    console.log(`Hello, My name is ${this.name}`)
+  }
+
 }
 
 const max = new Person('Max');
-console.log(max)
+max.greeting();
+
+const anotherMax = {
+  name: 'anotherMax',
+  anotherGreeting: max.greeting
+}
+
+anotherMax.anotherGreeting();
