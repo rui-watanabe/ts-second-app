4709e7f250aa3c2290e077190d0a049f3f75ef7d
rui-watanabe <wataru610806@gmail.com>
2020-05-30 17:53:44 +0900

    create class with generics

--- src/generics.ts
+++ src/generics.ts
@@ -4,5 +4,24 @@ function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): 
 }
 // console.log(copy<string>('hello'));
 console.log(copy({ name: 'Max' , age: 38}, 'age'));
+type K = keyof { name: string, age: number };
 
-type K = keyof { name: string, age: number }
+class LightDatabase <T extends string | number | boolean>{
+  private data: T[] = [];
+  add(item: T){
+    this.data.push(item);
+  }
+  remove(item: T){
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get(){
+    return this.data
+  }
+
+}
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('banana');
+stringLightDatabase.add('apple');
+stringLightDatabase.add('grape');
+stringLightDatabase.remove('apple');
+console.log(stringLightDatabase.get());
