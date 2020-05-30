d9ab5b2a553d817280827a09fa2cb1ec627ed733
rui-watanabe <wataru610806@gmail.com>
2020-05-30 18:21:51 +0900

    use bilt-in type

--- src/generics.ts
+++ src/generics.ts
@@ -34,3 +34,24 @@ const tmpDatabase: TmpDatabase<number> = {
   id: 3,
   data: [32, 34]
 }
+
+interface Todo {
+  title: string;
+  text: string;
+}
+type TodoAble = Partial<Todo>
+type ReadTodo = Readonly<Todo>
+
+// const fetchData: Promise<string> = new Promise(resolve => (
+//   setTimeout(() => {
+//     resolve('hello');
+//   }, 3000)
+// ));
+
+// fetchData.then(data => {
+//   data.toUpperCase();
+// })
+
+const vegetables1: string[] = ['Tomato', 'Broccoli', 'Asparagus'];
+const vegetables2: Array<string> = ['Tomato', 'Broccoli', 'Asparagus'];
+
