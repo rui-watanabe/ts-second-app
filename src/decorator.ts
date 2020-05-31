569cb477947694e0ba4ed36ce1cd88e369faf2d4
rui-watanabe <wataru610806@gmail.com>
2020-05-31 10:35:35 +0900

    use decorators

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,11 +1,26 @@
 // decorator is executed when class is defined
 function Logging (message: string) {
+  console.log('Executed Logging decorator factory');
   return function (constructor: Function) {
     console.log(message);
     console.log(constructor);
   } 
 }
 
+function Component (template: string, selector: string) {
+  console.log('Executed Component decorator factory');
+  return function (constructor: { new(...args: any[]): { name: string } }) {
+    const mountedElement = document.querySelector(selector);
+    const instance = new constructor();
+    if(mountedElement){
+      mountedElement.innerHTML = template;
+      mountedElement.querySelector('h1')!.textContent = instance.name;
+    }
+  }
+}
+
+// component decorator factory > logging decorator factory > component decorator > logging decorator
+@Component('<h1>{{ name}}</h1>','#app')
 @Logging('Logging User')
 class User {
   name = 'max';
