abed75685a2a72a6bdb8d1b369125a58ce3482d9
rui-watanabe <wataru610806@gmail.com>
2020-05-31 12:32:08 +0900

    hold all foods with foods class

---
+++ src/food-app.ts
@@ -0,0 +1,12 @@
+class Score {}
+class Food {}
+class Foods {
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+  constructor(){
+    this.elements.forEach(element => {
+
+    });
+  }
+}
+
+const foods = new Foods();
