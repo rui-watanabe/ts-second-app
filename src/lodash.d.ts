48c8b1599a10853ab7d1c5a2727dbae9dec3cc34
rui-watanabe <wataru610806@gmail.com>
2020-06-02 19:10:22 +0900

    set .d.ts manual from cdn

--- src/lodash.d.ts
+++ src/lodash.d.ts
@@ -1,3 +1,10 @@
 declare module 'lodash' {
   export function shuffle<T>(arr: T[]): T[];
 }
+// declare const _: {
+//   shuffle<T>(arr: T[]): T[];
+// }
+interface Lodash {
+  shuffle<T>(arr: T[]): T[];
+}
+declare const _: Lodash;
