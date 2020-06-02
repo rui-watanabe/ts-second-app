fa76c1b180a52af24ce7c7c3c9bd48f7594d64d6
rui-watanabe <wataru610806@gmail.com>
2020-06-02 19:23:01 +0900

    define inteface in namespace

--- src/library.ts
+++ src/library.ts
@@ -3,7 +3,12 @@
 // import _ from 'lodash';
 console.log(_.shuffle([1, 2, 3, 4]));
 namespace myApp {
-  const hello = 'hello in namespace'
+  const hello = 'hello in namespace';
   export const name ='max';
+  export interface Nameable {
+    name: string;
+  }
 }
-const hello = myApp.name
+// const hello = myApp.name;
+let nameable: myApp.Nameable;
+
